9de0fb9acdf48166a4922a8e26b5ada164540bba
aska <asya.zaharova@gmail.com>
2013-06-30 18:22:06 +0400

    js

--- code/js/common.js
+++ code/js/common.js
@@ -22,13 +22,15 @@ $( window ).resize( function() {
 
 
 function FrameSize() {
-    iWindowHeight = $( window ).height();    
+    iWindowHeight = $( window ).height();
     iFrameBorderTopHeight = $ ( '.frame' ).css( 'padding-top' ).replace('px','');
     iFrameBorderBottomHeight = $ ( '.frame' ).css( 'padding-bottom' ).replace('px','');
     ScreenSizeHeight =  iWindowHeight - iFrameBorderTopHeight - iFrameBorderBottomHeight;
     $ ( '.screen' ).height ( ScreenSizeHeight  - 34 );
     $( '.thumbnail_single-work' ).height ( ScreenSizeHeight  - 34 );
-    
+
 }
 
+function  WorkImageSize() {
 
+}
